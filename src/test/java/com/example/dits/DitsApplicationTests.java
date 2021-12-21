6ccba7ec4090bc26df05da8314000028b8defd9f
Evgeny Levdorovich <39227373+levjack1995@users.noreply.github.com>
2021-12-21 12:37:52 +0300

    Initial commit

---
+++ src/test/java/com/example/dits/DitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
